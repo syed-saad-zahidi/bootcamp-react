6952d39d7bfd0ad9e068c196ad1db04d2fef180b
syed-saad-zahidi <59394889+syed-saad-zahidi@users.noreply.github.com>
2020-06-09 22:09:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <h1>hello world from all of us !</h1>
+   <h1>hello world from SYED SAAD ZAHIDI</h1>
     </div>
   );
 }
